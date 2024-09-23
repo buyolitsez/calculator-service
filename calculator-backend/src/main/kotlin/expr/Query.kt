1f02865ea807d94a53b3863f4561210515c6b859
AntonHolden <robsmt@mail.ru>
2024-09-23 20:56:09 +0300

    Update Query.kt

--- calculator-backend/src/main/kotlin/expr/Query.kt
+++ calculator-backend/src/main/kotlin/expr/Query.kt
@@ -1,25 +1,39 @@
 package com.github.heheteam.expr
 
+import com.github.heheteam.Database.Companion.openOrCreateDatabase
+import com.github.heheteam.Entry
+import com.github.heheteam.databasePath
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.http.content.*
 import io.ktor.server.request.*
+import java.io.File
 
 const val request = "/calculator"
+const val requestHistory = "/history"
 
 fun Application.configureRouting() {
+
+    val database = openOrCreateDatabase(databasePath).value // TODO: handle error
+
 
     install(ContentNegotiation) {
         json()
     }
 
     routing {
+        staticFiles("/", File("static"), index = "index.html")
+    }
+
+    routing {
 
         post (request) {
-            val expr = call.receiveText()
+            val requestBody = call.receive<Map<String, String>>() // Read JSON as a map
+            val expr = requestBody["expression"] ?: ""
 
             val ast = parseExpr(expr)
 
@@ -36,6 +50,14 @@ fun Application.configureRouting() {
             }
 
             call.respond(result.value)
+            database.appendEntry(Entry(expr, result))
+        }
+    }
+
+    // TODO: this shit doesn't work :-(
+    routing {
+        post (requestHistory) {
+            call.respond(database)
         }
     }
 }
