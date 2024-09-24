091fff94877d3c122ad35799f56742f58744f45d
Nosorozhek <nosorozhek239@gmail.com>
2024-09-24 11:19:49 +0300

    Added whitespaces around operators in history (except unary minus)

--- calculator-frontend/scripts/history.js
+++ calculator-frontend/scripts/history.js
@@ -25,19 +25,18 @@ window.onload = getHistory;
 function addToHistoryTable(exprString, value, ind = 0) {
     const row = historyTable.insertRow(ind);
     const exprCell = row.insertCell();
-    exprCell.classList.add("is_clickable");
     const expr = document.createElement('span');
-    expr.textContent = exprString + ' = ' + value;
+    expr.classList.add("is-clickable");
+    expr.setAttribute("data-expression", exprString)
+    expr.textContent = (exprString + ' = ' + value).replaceAll(/([\d)])([=*/+-])/g, "$1 $2 ");
     expr.classList.add("expression");
     exprCell.appendChild(expr);
-    exprCell.onclick = getFromHistoryTable;
+    expr.onclick = getFromHistoryTable;
 }
 
 /**
  * gets expression from html table to input field
  */
 function getFromHistoryTable(e) {
-    const exprAndValue = e.target.textContent.split('=');
-    console.assert(exprAndValue.length === 2);
-    input.value = exprAndValue[0].trim();
+    input.value = e.target.getAttribute("data-expression");
 }
